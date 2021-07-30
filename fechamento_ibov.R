de6eddc342f791c1b0acf3158bee32c5a598d951
tatsukib <tatsukito@gmail.com>
2021-07-30 14:53:57 -0300

    deletei linha

--- fechamento_ibov.R
+++ fechamento_ibov.R
@@ -3,6 +3,7 @@ library(tidyverse)
 library(openxlsx)
 library(quantmod)
 
+
 tickers = c('^BVSP','VALE3.SA','GOAU4.SA','SUZB3.SA','VIVT3.SA','MELI34.SA','QUAL3.SA','PRIO3.SA','MGLU3.SA','BPAC11.SA','BRDT3.SA','LIGT3.SA','HYPE3.SA',
             'KLBN11.SA','WEGE3.SA','RENT3.SA','ENEV3.SA','ENGI11.SA','TRPL4.SA','FLRY3.SA','CPFE3.SA','B3SA3.SA','LREN3.SA','TOTS3.SA','RADL3.SA')
 
@@ -22,7 +23,6 @@ dt <- t(dt)
 
 
 # escreve num arquivo xlsx
-write.xlsx(dt,"C:\\Users\\55119\\OneDrive\\?rea de Trabalho\\Datasets\\Mercado\\Ibov.xlsx", col.names = F,row.names=T)
 
+write.xlsx(dt,"C:\\Users\\55119\\OneDrive\\Área de Trabalho\\Datasets\\Mercado\\Ibov.xlsx", col.names = F,row.names=T)
 
-#chartSeries(TRPL4.SA, subset = 'last 3 months')
