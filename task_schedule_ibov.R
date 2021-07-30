477ffc086c5e6627837c06715feda1cbdd7eadc9
tatsukib <tatsukito@gmail.com>
2021-07-30 14:08:08 -0300

    task schedule

--- task_schedule_ibov.R
+++ task_schedule_ibov.R
@@ -4,3 +4,4 @@ myscript <- system.file("extdata", "fechamento_ibov.R", package = "taskscheduleR
 
 taskscheduler_create(taskname = "myfancyscript_sunsat", rscript = myscript, 
                      schedule = "WEEKLY", starttime = "09:00", days = c('SUN', 'SAT'))
+
