1b01694bb452842c626842e6a54e0fb40634b94c
tatsukib <tatsukito@gmail.com>
2021-07-30 14:50:33 -0300

    deletei comentário

--- task_schedule_ibov.R
+++
@@ -1,7 +0,0 @@
-library(taskscheduleR)
-
-myscript <- system.file("extdata", "fechamento_ibov.R", package = "taskscheduleR")
-
-taskscheduler_create(taskname = "myfancyscript_sunsat", rscript = myscript, 
-                     schedule = "WEEKLY", starttime = "09:00", days = c('SUN', 'SAT'))
-
